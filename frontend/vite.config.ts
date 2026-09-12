96179b6743023107d548f304f054e5dfab05c6a9
bernardosgtmatos <bernardosgtmatos@gmail.com>
2026-09-11 21:05:38 -0300

    add proxy /cliente + /admin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/Cliente': 'http://localhost:5000',
+      '/Admin': 'http://localhost:5000',
+    },
+  },
 })
