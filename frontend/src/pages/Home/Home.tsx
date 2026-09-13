c9a62ae7a625430dabb68888e4724d1804cff80a
bernardosgtmatos <bernardosgtmatos@gmail.com>
2026-09-13 17:36:33 -0300

    Hud responsivo para celular

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -8,8 +8,8 @@ function Home() {
         <div className="ellipse ellipse-1" />
         <div className="ellipse ellipse-2" />
         <div className="ellipse ellipse-3" />
+        <h1 className="home-title">Choque Sabor</h1>
       </div>
-      <h1 className="home-title">Choque Sabor</h1>
       <Link to="/cardapio" className="home-cta">
         Ver Cardápio
       </Link>
